751074873df2327e0b7611200db5fc71caa98156
sonu28gpt <98702109+sonu28gpt@users.noreply.github.com>
2025-01-21 22:38:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	
+		this.name=name;
+		this.age=age;
+	
+	
+}
+Person.prototype.greet=function(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+}
 
-function Employee(name, age, jobTitle) {}
+
+
+
+function Employee(name, age, jobTitle) {
+	let obj=Person(name,age);
+	obj.jobTitle=jobTitle;
+	obj.jobGreet=function () {
+		console.log(`Hello, my name is ${obj.name}, I am ${obj.age} years old, and my job title is ${obj.jobTitle}.`)
+	}
+	return obj;
+}
 
 // Do not change code below this line
 window.Person = Person;
